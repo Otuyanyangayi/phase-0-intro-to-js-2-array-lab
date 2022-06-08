2daf3304b8ad79a05336afd1437b981a80986c8f
Grace <gracenyangayi@gmail.com>
2022-06-08 10:46:53 +0300

    commit tree 1bf2d7cca47a82933cd7e03048ca47d13c9a5129

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield" ];
+function destructivelyAppendCat(name){
+    cats.push(name);
+    return cats;
+}
+function destructivelyPrependCat(name){
+    cats.unshift(name);
+    return cats;
+}
+function destructivelyRemoveFirstCat(){
+    cats.shift()
+    return cats;
+}
+function destructivelyRemoveLastCat(){
+    cats.pop();
+    return cats;  
+}
+function appendCat(name){
+    const newArray5 = [...cats, name]
+    return newArray5;
+}
+
+function prependCat(name){
+    const newArray2 = [name,...cats ]
+    return newArray2;
+}
+function removeLastCat(){
+    const newArray3 = cats.slice(0,cats.length-1)
+    return newArray3;
+}
+function removeFirstCat(){
+    const newArray4 = cats.slice(1);
+    return newArray4;
+}
+
